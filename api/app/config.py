a1401619ab481798021115e94aca08ee01f62a43
Laxminarayan Raval <ln.raval01@gmail.com>
2023-02-16 18:21:06 +0530

    configuration setup

---
+++ api/app/config.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path(".") + ".env"
+load_dotenv(dotenv_path=env_path)
+
+
+class Settings:
+    PROJECT_NAME: str = "Jira Clone lx"
+    PROJECT_VERSION: str = "1.0.0"
+
+    POSTGRES_USER: str = os.getenv("DB_USER")
+    POSTGRES_PASSWORD = os.getenv("DB_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("DB_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("DB_PORT", 5432)
+    POSTGRES_DB: str = os.getenv("DB_NAME")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+settings = Settings()
