f7d511844e1cdeaf875518a785087debf9c047ca
Laxminarayan Raval <ln.raval01@gmail.com>
2023-02-22 13:16:45 +0530

    google oauth config and package added; env update

--- api/app/config.py
+++ api/app/config.py
@@ -1,13 +1,29 @@
 import os
 
+google_credentials = {
+    "web": {
+        "client_id": "1059122565838-ssslohu20habg3ie5g66ov750jo7dq0p.apps.googleusercontent.com",
+        "project_id": "jiraclone-378110",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": "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",
+        "redirect_uris": [
+            "http://127.0.0.1:8008/",
+            "http://127.0.0.1:8008/login/google/callback",
+        ],
+        "javascript_origins": ["http://127.0.0.1:8008"],
+    }
+}
+
 
 class Settings:
     PROJECT_NAME: str = "Jira Clone lx"
     PROJECT_VERSION: str = "1.0.0"
 
-    POSTGRES_USER: str = os.getenv("DB_USER")
+    POSTGRES_USER: str = os.getenv("DB_USERNAME")
     POSTGRES_PASSWORD = os.getenv("DB_PASSWORD")
-    POSTGRES_SERVER: str = os.getenv("DB_SERVER", "localhost")
+    POSTGRES_SERVER: str = os.getenv("DB_HOST", "localhost")
     POSTGRES_PORT: str = os.getenv("DB_PORT", 5432)
     POSTGRES_DB: str = os.getenv("DB_NAME")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
